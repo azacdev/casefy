bcbf9db9d7d5c20ed8d673a2297f511b8c04ee32
azacdev <azacdev@gmail.com>
2024-05-11 16:00:54 +0100

    build: hero section

--- app/chat/[chatId]/page.tsx
+++
@@ -1,9 +0,0 @@
-interface ChatIdPageProps {
-  params: { chatId: string };
-}
-
-const ChatIdPage = ({ params }: ChatIdPageProps) => {
-  return <div className="text-white">{params.chatId}</div>;
-};
-
-export default ChatIdPage;
