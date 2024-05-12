09424b7730a76c05e398df1ffcd115fadcad848d
azacdev <azacdev@gmail.com>
2024-05-12 16:25:30 +0100

    build: landing page

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { SessionProvider } from "next-auth/react";
 
 import "@/styles/globals.css";
 import Navbar from "@/components/navbar";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,6 +24,7 @@ export default async function RootLayout({
         <SessionProvider>
           <Navbar />
           {children}
+          <Footer />
         </SessionProvider>
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
-import { Check, Star } from "lucide-react";
+import { ArrowRight, Check, Star } from "lucide-react";
 import Image from "next/image";
 
 import Phone from "@/components/phone";
 import MaxWidthWrapper from "@/components/max-width-wrapper";
 import { Icons } from "@/components/icons";
 import Reviews from "@/components/reviews";
+import Link from "next/link";
+import { buttonVariants } from "@/components/ui/button";
 
 export default function Home() {
   return (
@@ -42,7 +44,7 @@ export default function Home() {
                     High-quality, durable material
                   </li>
                   <li className="flex gap-1.5 items-center text-left">
-                    <Check className="h-5 w-5 shrink-0 text-green-600" />3 year
+                    <Check className="h-5 w-5 shrink-0 text-green-600" />5 year
                     print guarantee
                   </li>
                   <li className="flex gap-1.5 items-center text-left">
@@ -124,8 +126,8 @@ export default function Home() {
       </section>
 
       <section className="bg-slate-100 py-24">
-        <MaxWidthWrapper className="flex flex-col lg:flex-row items-center gap-16 sm:gap-32">
-          <div className="flex flex-col lg:flex-row items-center gap-16 sm:gap-32">
+        <MaxWidthWrapper className="flex flex-col items-center gap-16 sm:gap-32">
+          <div className="flex flex-col lg:flex-row items-center gap-4 sm:gap-6">
             <h2 className="order-1 mt-2 tracking-tight text-center text-balance !leading-tight font-bold text-5xl md:text-6xl text-gray-900">
               What our{" "}
               <span className="relative px-2">
@@ -226,9 +228,80 @@ export default function Home() {
           </div>
         </MaxWidthWrapper>
 
-        <div className="pt-16">
+        <div className="pt-16 overflow-hidden">
           <Reviews />
         </div>
+      </section>
+
+      <section>
+        <MaxWidthWrapper className="py-24">
+          <div className="mb-12 px-6 lg:px-8">
+            <div className="mx-auto max-w-2xl sm: text-center">
+              <h2 className="order-1 mt-2 tracking-tight text-center text-balance !leading-tight font-bold text-5xl md:text-6xl text-gray-900">
+                Upload your photo and get your{" "}
+                <span className="relative px-2 bg-green-600 text-white rounded-md">
+                  own case
+                </span>{" "}
+                now
+              </h2>
+            </div>
+          </div>
+
+          <div className="mx-auto max-w-6xl px-6 lg:px-8">
+            <div className="relative flex flex-col items-center md:grid grid-cols-2 gap-40">
+              <Image
+                src="/arrow.png"
+                className="absolute top-[25rem] md:top-1/2 -translate-y-1/2 z-10 left-1/2 -translate-x-1/2 rotate-90 md:rotate-0"
+                width={100}
+                height={100}
+                alt="arrow-icon"
+              />
+
+              <div className="relative h-80 md:h-full w-full md:justify-self-end max-w-sm rounded-xl bg-gray-900/5 ring-inset ring-gray-900/10 lg:rounded-2xl">
+                <Image
+                  src="/horse.jpg"
+                  className="rounded-md object-cover bg-white shadow-2xl ring-1 ring-gray-900/10 h-full w-full"
+                  width={1000}
+                  height={1000}
+                  alt="horse-image"
+                />
+              </div>
+
+              <Phone className="w-60" imgSrc="/horse_phone.jpg" />
+            </div>
+          </div>
+
+          <ul className="mx-auto mt-12 max-w-prose sm:text-lg space-y-2 w-fit">
+            <li className="w-fit">
+              <Check className="h-5 w-5 text-green-600 inline mr-1.5" />
+              High-quality silicone material
+            </li>
+            <li className="w-fit">
+              <Check className="h-5 w-5 text-green-600 inline mr-1.5" />
+              Scratch- and fingerprint resistant coating
+            </li>
+            <li className="w-fit">
+              <Check className="h-5 w-5 text-green-600 inline mr-1.5" />
+              Wireless charging compatible
+            </li>
+            <li className="w-fit">
+              <Check className="h-5 w-5 text-green-600 inline mr-1.5" />5 year
+              print warranty
+            </li>
+
+            <div className="flex justify-center">
+              <Link
+                className={buttonVariants({
+                  size: "lg",
+                  className: "mx-auto mt-8",
+                })}
+                href="/configure/upload"
+              >
+                Create your case now <ArrowRight className="h-4 w-4 ml-1.5" />
+              </Link>
+            </div>
+          </ul>
+        </MaxWidthWrapper>
       </section>
     </main>
   );
