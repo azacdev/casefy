0202b8ca263f90abf63e46a5b0da70c20223f485
azacdev <azacdev@gmail.com>
2024-05-21 01:57:15 +0100

    feat: configure action

--- actions/create-checkout-session.tsx
+++ actions/create-checkout-session.tsx
@@ -3,6 +3,7 @@
 import { BASE_PRICE, PRODUCT_PRICES } from "@/config/products";
 import db from "@/lib/db";
 import getSession from "@/lib/get-session";
+import { Order } from "@prisma/client";
 
 const CreateCheckoutSession = async ({ configId }: { configId: string }) => {
   const configuration = await db.configuration.findUnique({
@@ -26,6 +27,29 @@ const CreateCheckoutSession = async ({ configId }: { configId: string }) => {
   if (finish === "textured") price += PRODUCT_PRICES.finish.textured;
   if (material === "polycarbonate")
     price += PRODUCT_PRICES.material.polycarbonate;
+
+  let order: Order | undefined = undefined;
+
+  const existingOrder = await db.order.findFirst({
+    where: {
+      userId: user.id,
+      configurationId: configuration.id,
+    },
+  });
+
+  if (existingOrder) {
+    order = existingOrder;
+  } else {
+    order = await db.order.create({
+      data: {
+        amount: price / 100,
+        userId: user.id!,
+        configurationId: configuration.id,
+      },
+    });
+  }
+
+  
 };
 
 export default CreateCheckoutSession;
